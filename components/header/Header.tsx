7dac6e720c13bc40dc0c903aa7d8c6c1107e2c7d
Camel-app <camel.app@proton.me>
2024-05-14 11:37:41 +0200

    add camel to header

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -7,6 +7,7 @@ const links = [
   { link: '/', label: 'Home', target: "_self"},
   { link: '/information', label: 'Information', target: "_self"},
   { link: 'https://osf.io/q5hj4/', label: 'Documentation', target: "_blank"},
+  { link: 'https://camgalaxy.github.io/', label: 'C.A.M.E.L. (participant view)', target: "_blank"},
   { link: 'https://fennapps.shinyapps.io/CAMtools_CAMapp/', label: 'CAM-App', target: "_blank"},
   { link: '/register', label: 'Register', target: "_self"},
   { link: '/login', label: 'Log in' },
