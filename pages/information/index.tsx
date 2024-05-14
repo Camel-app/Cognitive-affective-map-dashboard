60e26794945fccd5154fc5513c71fff46b31e9a7
Camel-app <camel.app@proton.me>
2024-05-14 11:33:08 +0200

    add information page

---
+++ pages/information/index.tsx
@@ -0,0 +1,15 @@
+import { Information } from '@/components/Information/Information';
+import { HeaderSimple } from '../../components/Header/Header';
+
+
+
+
+
+export default function HomePage() {
+  return (
+    <>
+      <HeaderSimple />
+      <Information />
+    </>
+  );
+}
